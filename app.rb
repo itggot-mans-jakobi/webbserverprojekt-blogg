f09b9668e7e09593a489b1ab22975a3aa1f68769
itggot-mans-jakobi <38528626+itggot-mans-jakobi@users.noreply.github.com>
2019-03-07 10:54:57 +0100

    Fixed loggin and started on a new userpage system
    Fixed loggin and started on a new userpage system

--- app.rb
+++ app.rb
@@ -9,37 +9,52 @@ get("/") do
     db = SQLite3::Database.new("db/database.db")
     db.results_as_hash = true
     
-    result = db.execute("SELECT PostId, PostText FROM posts")
+    result = db.execute("SELECT PostId, PostText, Username FROM posts")
 
     slim(:index, locals:{
         post: result})
 end
 
-get("/user") do
-    slim(:user)
+get("/:user") do
+    db = SQLite3::Database.new("db/database.db")
+    db.results_as_hash = true
+    # params["user"]
+
+    result = db.execute("SELECT PostId, PostText, Username FROM posts")
+
+    slim(:userpage, locals:{
+        post: result,
+        username_viev: params["user"]
+    })
+   
 end
 
 post("/user_login") do
     username = params["username"]
     password = params["password"]
-    hashat_password = BCrypt::Password.create(password)
+    
 
     db = SQLite3::Database.new("db/database.db")
     db.results_as_hash = true
-
+    if password == ""
+        redirect("/")
+    end
    
     if params["submit_button"] == "Login"
-        result_password = db.execute("SELECT Password FROM users where Username = 'username'")
-        if hashat_password == result_password[0][0]
+        result_password = db.execute("SELECT Password FROM users WHERE Username=?", username)
+        if BCrypt::Password.new(result_password[0]["Password"]) == password
             session[:logged_in] = true
             session[:user] = username
             
             redirect("/")
+        # else
+        #     redirect("/")
         end
     end
-
     if params["submit_button"] == "Create user"
-        db.execute("INSERT INTO users (Username, Password) VALUES (?,?)", username, hashat_password)
+        db.execute("INSERT INTO users (Username, Password) VALUES (?,?)", username, BCrypt::Password.create(password))
+        session[:logged_in] = true
+        session[:user] = username
     end
 
     redirect("/")
@@ -48,9 +63,22 @@ end
 
 post("/user_logout") do
     session[:logged_in] = false
+    session[:user] = ""
     redirect("/")
 end
 
 post("/post_new") do
+    post_text = params["post_text"]
+    username_post = session[:user]
+    if post_text == ""
+        redirect("/user")
+    end
 
+    db = SQLite3::Database.new("db/database.db")
+    db.results_as_hash = true    
+
+    post_text = params["post_text"]
+    db.execute("INSERT INTO posts (Username, PostText) VALUES (?,?)", username_post, post_text)
+
+    redirect("/")
 end
